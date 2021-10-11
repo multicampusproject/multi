9deb5f3a9fa7916c3289068acc6f8d45738dc00d
woosoo1230 <uusuu1230@gmail.com>
2021-10-11 15:01:11 +0900

    설정

---
+++ src/main/java/edu/spring/multi/MulticampusprojectApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticampusprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticampusprojectApplication.class, args);
+	}
+
+}
