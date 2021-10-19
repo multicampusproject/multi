1a88a467943023e15c4fb214893f8ee01b382bad
woosoo1230 <uusuu1230@gmail.com>
2021-10-19 09:19:57 +0900

    commit test - woo

--- src/main/java/edu/spring/multi/MulticampusprojectApplication.java
+++ src/main/java/edu/spring/multi/MulticampusprojectApplication.java
@@ -9,6 +9,7 @@ public class MulticampusprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MulticampusprojectApplication.class, args);
 		System.out.println("import 완료");
+		System.out.println("commit test");
 	}
 
 }
